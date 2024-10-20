311c91f3d1696967717bfdcf4cd28153cfe5e90b
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-10-20 08:56:16 +0330

    add app home

---
+++ core/home/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+class HomeView(APIView):
+    def get(self, request):
+        return Response({'hi': 'hello world'})
