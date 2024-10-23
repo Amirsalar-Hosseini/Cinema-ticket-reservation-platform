08b8ae89beba0644ee6f0cbcfaf1cdd787e7cf4e
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-10-23 11:29:45 +0330

    cinema app models are add to admin display

--- core/cinema/admin.py
+++ core/cinema/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Location, Screen, Cinema
 
-# Register your models here.
+
+@admin.register(Cinema)
+class CinemaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'location', 'address', 'num_of_screens', 'total_capacity']
+
+
+@admin.register(Screen)
+class ScreenAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cinema', 'screen_number', 'capacity']
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'city', 'province']
