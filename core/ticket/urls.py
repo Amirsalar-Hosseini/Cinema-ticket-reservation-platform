42051733dafa391ab61b943b7a125a2e512b1e06
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-10-23 17:46:10 +0330

    the basic views of ticket app were create

--- core/ticket/urls.py
+++ core/ticket/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
+from .views import PaymentView, ShowtimeView, TicketView, ReservationView, PriceCategoryView
 
 app_name = 'ticket'
-urlpatterns = []
+urlpatterns = [
+    path('', TicketView.as_view(), name='ticket'),
+    path('payment/', PaymentView.as_view(), name='payment'),
+    path('reservation/', ReservationView.as_view(), name='reservation'),
+    path('showtime/', ShowtimeView.as_view(), name='showtime'),
+    path('pricecategory/', PriceCategoryView.as_view(), name='pricecategory'),
+]
--- core/ticket/views.py
+++ core/ticket/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Showtime, Ticket, Reservation, Payment, PriceCategory
+from .serializers import ShowtimeSerializer, TicketSerializer, ReservationSerializer, PaymentSerializer, \
+    PriceCategorySerializer
 
-# Create your views here.
+
+class ShowtimeView(APIView):
+    queryset = Showtime.objects.all()
+    serializer_class = ShowtimeSerializer
+
+    def get(self, request):
+        showtimes = self.queryset.all()
+        ser_data = self.serializer_class(showtimes, many=True)
+        return Response(ser_data.data)
+
+
+class TicketView(APIView):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+    def get(self, request):
+        tickets = self.queryset.all()
+        ser_data = self.serializer_class(tickets, many=True)
+        return Response(ser_data.data)
+
+
+class ReservationView(APIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+    def get(self, request):
+        reservations = self.queryset.all()
+        ser_data = self.serializer_class(reservations, many=True)
+        return Response(ser_data.data)
+
+
+class PaymentView(APIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    def get(self, request):
+        payments = self.queryset.all()
+        ser_data = self.serializer_class(payments, many=True)
+        return Response(ser_data.data)
+
+
+class PriceCategoryView(APIView):
+    queryset = PriceCategory.objects.all()
+    serializer_class = PriceCategorySerializer
+
+    def get(self, request):
+        price_categories = self.queryset.all()
+        ser_data = self.serializer_class(price_categories, many=True)
+        return Response(ser_data.data)
