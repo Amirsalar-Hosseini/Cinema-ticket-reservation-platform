580edfba000896d8d2f9b74b77c8013e0a54ae61
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-10-23 12:25:27 +0330

    movie app models are add to admin display

--- core/movie/admin.py
+++ core/movie/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.templatetags.i18n import language
 
-# Register your models here.
+from .models import Movie, Review, Genre
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'release_date', 'director', 'language', 'age_rating']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'movie', 'user', 'rating', 'comment', 'created_at']
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
