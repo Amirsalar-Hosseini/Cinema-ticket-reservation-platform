46a870160878f0196e905976f7d90bd72ea80dd8
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-10-23 16:37:37 +0330

    the basic views of cinema app were create

--- core/cinema/urls.py
+++ core/cinema/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from .views import CinemaView, ScreenView, LocationView
 
 app_name='cinema'
-urlpatterns = []
+urlpatterns = [
+    path('', CinemaView.as_view(), name='cinema'),
+    path('screen/', ScreenView.as_view(), name='screen'),
+    path('location/', LocationView.as_view(), name='location'),
+]
--- core/cinema/views.py
+++ core/cinema/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Cinema, Screen, Location
+from .serializers import CinemaSerializer, ScreenSerializer, LocationSerializer
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class CinemaView(APIView):
+    queryset = Cinema.objects
+    serializer_class = CinemaSerializer
+
+    def get(self, request):
+        cinemas = self.queryset.all()
+        serializer = self.serializer_class(cinemas, many=True)
+        return Response(serializer.data)
+
+
+class ScreenView(APIView):
+    queryset = Screen.objects
+    serializer_class = ScreenSerializer
+
+    def get(self, request):
+        screens = self.queryset.all()
+        serializer = self.serializer_class(screens, many=True)
+        return Response(serializer.data)
+
+class LocationView(APIView):
+    queryset = Location.objects
+    serializer_class = LocationSerializer
+
+    def get(self, request):
+        locations = self.queryset.all()
+        serializer = self.serializer_class(locations, many=True)
+        return Response(serializer.data)
+
