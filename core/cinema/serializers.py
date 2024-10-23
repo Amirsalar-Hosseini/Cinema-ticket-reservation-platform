d2777233458f83833ff1fe07fff500e6a3747a11
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-10-23 10:55:03 +0330

    define cinema app serializers

---
+++ core/cinema/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Cinema, Screen, Location
+
+
+class CinemaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cinema
+        fields = '__all__'
+
+
+class ScreenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Screen
+        fields = '__all__'
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = '__all__'
