d564be0aa611bf10e1e59a87eab6de771cc1834d
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-10-25 22:39:08 +0330

    cinema app views and serializers with urls were not needed

--- core/cinema/urls.py
+++ core/cinema/urls.py
@@ -1,9 +1,3 @@
-from django.urls import path
-from .views import CinemaView, ScreenView, LocationView
 
 app_name='cinema'
-urlpatterns = [
-    path('', CinemaView.as_view(), name='cinema'),
-    path('screen/', ScreenView.as_view(), name='screen'),
-    path('location/', LocationView.as_view(), name='location'),
-]
+urlpatterns = []
