535792aa2e6617f9378f70a5d1fc011dbbf64eda
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-10-28 12:40:46 +0330

    filter genres was added and users can comment for movie

--- core/movie/urls.py
+++ core/movie/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import ReviewView, MovieView, MovieDetailView
+from .views import MovieView, MovieDetailView
 
 app_name = 'movie'
 urlpatterns = [
     path('', MovieView.as_view(), name='movie'),
     path('<int:pk>/', MovieDetailView.as_view(), name='movie_detail'),
-    path('review/', ReviewView.as_view(), name='review'),
 ]
--- core/movie/views.py
+++ core/movie/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Movie, Review
+from rest_framework import status
+from accounts.models import User
+from .models import Movie, Review, Genre
 from .serializers import MovieSerializer, ReviewSerializer
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -13,7 +15,11 @@ class MovieView(APIView):
     serializer_class = MovieSerializer
 
     def get(self, request):
+        genres = request.query_params.getlist('genres', None)
         movies = self.queryset.all()
+        if genres:
+            genres_name = Genre.objects.filter(name__in=genres)
+            movies = movies.filter(genre__in=genres_name).distinct()
         paginator = self.pagination_class()
         paginator.page_size = 10
         paginated_movies = paginator.paginate_queryset(movies, request)
@@ -21,7 +27,7 @@ class MovieView(APIView):
         return paginator.get_paginated_response(ser_data.data)
 
 class MovieDetailView(APIView):
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
@@ -30,5 +36,11 @@ class MovieDetailView(APIView):
         ser_data = self.serializer_class(movie)
         return Response(ser_data.data)
 
+    def post(self, request, pk):
+        rating = request.data.get('rating')
+        comment = request.data.get('comment')
+        movie = Movie.objects.get(pk=pk)
+        user = User.objects.get(id=request.user.id)
+        Review.objects.create(user=user, rating=rating, comment=comment, movie=movie)
+        return Response({'SUCCESS': 'thanks for comment'}, status=status.HTTP_201_CREATED)
 
-# review for movie detail view POST
